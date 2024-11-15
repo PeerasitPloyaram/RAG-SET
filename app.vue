7fdbfd2d13332d189114af104b8a36d2a69ff420
Peerasit <peerasit.p@facgure.com>
2024-11-15 21:42:33 +0700

    Init static page

--- app.vue
+++ app.vue
@@ -1,10 +1,11 @@
 <template>
   <div>
     <NuxtRouteAnnouncer/>
-    <div>
+    <div style="display: flex; flex-direction: column; justify-content: center; align-items: center;">
       <h2>
         This is Productions
       </h2>
+      <p>Peerasit Ployaram</p>
     </div>
   </div>
 </template>
