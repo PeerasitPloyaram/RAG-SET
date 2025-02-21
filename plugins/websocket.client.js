db191dc6921b33440f08ced1d9ab19719c9ff925
Peerasit <peerasit.p@facgure.com>
2025-02-22 00:31:36 +0700

    Add Core Websocket for Recieve Notification.

---
+++ plugins/websocket.client.js
@@ -0,0 +1,28 @@
+export default defineNuxtPlugin((nuxtApp) => {
+  const config = useRuntimeConfig()
+  if (!process.client) return;
+
+  const wsUrl = `ws://${config.public.api_path}/ws/U1234`;
+  const socket = new WebSocket(wsUrl);
+  
+  socket.onopen = () => {
+    console.log("WebSocket Connected");
+  };
+
+  socket.onmessage = (event) => {
+    alert(`Notification: ${event.data}`);
+  };
+
+  socket.onerror = (error) => {
+    console.error("WebSocket error:", error);
+  };
+
+  socket.onclose = () => {
+    console.log("WebSocket closed Reconnecting...");
+    setTimeout(() => {
+      socket = new WebSocket(wsUrl);
+    }, 3000);
+  };
+
+  nuxtApp.provide("websocket", socket);
+});
