6581142ef9bf61f93a365ba04e68090c951992bc
Peerasit <peerasit.p@facgure.com>
2025-03-07 21:42:53 +0700

    Set User Session Expire to 30 minutes

--- composables/utils.ts
+++ composables/utils.ts
@@ -3,7 +3,7 @@ import { useCookie } from '#app';
 export const CookieStorageSetExpire = (key:string, value:string) => {
 
     const myCookie = useCookie(key, {
-        maxAge: 60,
+        maxAge: 60 * 30,
         secure: false,
         sameSite: 'strict'
     });
